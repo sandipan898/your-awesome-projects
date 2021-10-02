120e567244860bf67180a5de8d7994cd4963b1a0
mehulkundu <programmer.mehul@gmail.com>
2021-10-02 12:06:26 +0530

    react-task-tracker app addedd

---
+++ react-task-tracker/README.md
@@ -0,0 +1,25 @@
+## Usage
+
+### Install dependencies
+
+```
+npm install
+```
+
+### Run React dev server (http://localhost:3000)
+
+```
+npm start
+```
+
+### Run the JSON server (http://localhost:5000)
+
+```
+npm run server
+```
+
+### To build for production
+
+```
+npm run build
+```
---
+++ react-task-tracker/src/components/Tasks.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Task from './Task'
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <div>
+      {tasks.map((task, index) => (
+        <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
+      ))}
+    </div>
+  )
+}
+
+export default Tasks
